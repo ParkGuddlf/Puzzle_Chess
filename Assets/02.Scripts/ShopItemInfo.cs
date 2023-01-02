bbdb24260718ffc1beac94337662a896149c6684
rrrmaster <dudgus9101@gmail.com>
2023-01-02 22:50:08 +0900

    refactoring: 상점 리팩토링

--- Assets/02.Scripts/ShopItemInfo.cs
+++ Assets/02.Scripts/ShopItemInfo.cs
@@ -6,65 +6,83 @@ using UnityEngine.UI;
 public class ShopItemInfo : MonoBehaviour
 {
     public ObjectType[] objectType;
+
     //레벨에 따른 소환 정보
-    int length;
     public ObjectType currObjectType;
     [SerializeField] GameObject inventory;
     [SerializeField] GameObject buyEffect;
+    private int length;
+
+    private Button _button;
+    private Image _buttonImage;
+    private Text _pinNameText;
+    private Text _pinPriceText;
 
     private void Awake()
     {
+        _button = GetComponent<Button>();
+        _buttonImage = GetComponent<Image>();
+        _pinNameText = transform.GetChild(0).GetComponent<Text>();
+        _pinPriceText = transform.GetChild(1).GetComponent<Text>();
+
         //반복문으로 돌려서 집어넣는다
         for (int i = 0; i < PlayerInfo.instance.pinobjectTypes.Count; i++)
         {
             objectType[i] = PlayerInfo.instance.pinobjectTypes[i];
-        }        
+        }
     }
 
     private void Start()
     {
-        currObjectType = objectType[Random.Range(0, GameManager.Level*2)];
+        currObjectType = objectType[Random.Range(0, GameManager.Level * 2)];
     }
+
     //리롤하면 정보 초기화
     private void OnEnable()
     {
         //레벨에 맞는 리롤
-        currObjectType = objectType[Random.Range(0, GameManager.Level*2)];
-        GetComponent<Image>().sprite = currObjectType.pinSprite;
-        GetComponent<Button>().interactable = true;
+        currObjectType = objectType[Random.Range(0, GameManager.Level * 2)];
+
+        _buttonImage.sprite = currObjectType.pinSprite;
+        _button.interactable = true;
     }
+
     private void Update()
     {
         if (currObjectType != null)
         {
-            transform.GetChild(0).GetComponent<Text>().text = currObjectType.pinName;
-            transform.GetChild(1).GetComponent<Text>().text = currObjectType.price.ToString();
+            _pinNameText.text = currObjectType.pinName;
+            _pinPriceText.text = currObjectType.price.ToString();
         }
     }
+
     //버튼을 누르면 아이템의 정보를 인벤토리에 집어넣는다.
     public void BuyItem()
     {
         for (int i = 0; i < inventory.transform.childCount; i++)
         {
-            if (inventory.transform.GetChild(i).transform.childCount == 0 && GameManager.Money >= currObjectType.price)
+            Transform currParent = inventory.transform.GetChild(i);
+            if (currParent.transform.childCount == 0 && GameManager.Money >= currObjectType.price)
             {
-                GameObject spawnPin = Instantiate(currObjectType.pinPrefab, Vector3.zero, Quaternion.identity);
                 //가격에 따라 돈이 줄어든다.
                 GameManager.Money -= currObjectType.price;
                 //구입버튼 잠금
-                GetComponent<Button>().interactable = false;
-                //비어있는 창고로 넣는다
-                spawnPin.transform.SetParent(inventory.transform.GetChild(i));
-                GameObject spawn = Instantiate(buyEffect, inventory.transform.GetChild(i));
-                Destroy(spawn,0.5f);
-                //생성한 프리팹의 기본정보 변경
-                spawnPin.GetComponent<MouseDrag>().currParent = inventory.transform.GetChild(i);
-                spawnPin.GetComponent<MouseDrag>().objectType = currObjectType;
+                _button.interactable = false;
+                
+                GameObject spawnPin = Instantiate(currObjectType.pinPrefab, Vector3.zero, Quaternion.identity,currParent);
                 spawnPin.transform.localPosition = Vector3.zero;
+
+                //생성한 프리팹의 기본정보 변경
+                spawnPin.GetComponent<MouseDrag>().currParent = currParent;
+                spawnPin.GetComponent<MouseDrag>().objectType = currObjectType;
+                
+                //이펙트 생성
+                GameObject spawnEffect = Instantiate(buyEffect, currParent);
+                Destroy(spawnEffect, 0.5f);
+                
                 currObjectType = null;
                 break;
             }
         }
     }
-
 }
